b05219a7f6e93da76c4fb68e9a4f0300a00ff3e3
yuellian <yuelianxx@gmail.com>
2021-11-02 12:37:51 -0400

    11.2

---
+++ .history/stories/LearningIcon.stories_20211102122745.js
@@ -0,0 +1,38 @@
+import { html } from 'lit-html';
+
+import '../src/LCicon.js';
+
+export default {
+  title: 'Learning Icon',
+  component: 'learning-icon',
+  argTypes: {
+    type: {
+      control: 'select',
+      options: {
+        chem: 'chem',
+        objectives: 'objectives',
+        know: 'know',
+      },
+    },
+  },
+};
+
+// Learning Icon
+function LearningIconTemplate({ type }) {
+  return html` <learning-icon type=${type}></learning-icon>`;
+}
+
+export const ChemIcon = LearningIconTemplate.bind({});
+ChemIcon.args = {
+  type: 'chem',
+};
+
+export const KnowIcon = LearningIconTemplate.bind({});
+KnowIcon.args = {
+  type: 'know',
+};
+
+export const ObjectiveIcon = LearningIconTemplate.bind({});
+ObjectiveIcon.args = {
+  type: 'objectives',
+};
